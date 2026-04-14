3d89c24c2e4c9aa570c3ca7a952f9a55c8023443
Davli4 <vladislauskirey@yandex.by>
2026-04-15 01:49:28 +0300

    feat: Create main version

--- gateway/src/main/java/com/example/hotelpropertyview/HotelGatewayApplication.java
+++ gateway/src/main/java/com/example/hotelpropertyview/HotelGatewayApplication.java
@@ -1,4 +1,11 @@
 package com.example.hotelpropertyview;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class HotelGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HotelGatewayApplication.class, args);
+    }
 }
